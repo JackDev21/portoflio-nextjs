470a8b16aaec8e0b2889b6888351d1eb46462921
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-12-19 23:03:31 +0100

    adjust font text

--- portolio/src/app/components/Header/Navbar.tsx
+++ portolio/src/app/components/Header/Navbar.tsx
@@ -4,7 +4,7 @@ export function Navbar() {
   return (
     <nav
       id="start"
-      className="font-gaming flex items-center justify-center gap-6 text-[0.7rem] md:mr-14 md:flex-row md:justify-end md:gap-7 md:text-xl"
+      className="font-gaming flex items-center justify-center gap-6 text-[0.6rem] md:mr-14 md:flex-row md:justify-end md:gap-7 md:text-xl"
     >
       <Link href="#about" className="text-gray-900 hover:text-orange-500 dark:text-white dark:hover:text-orange-500">
         Sobre mí
